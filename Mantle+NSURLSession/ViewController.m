31a41bcc63320a290538bd14a39193f2bdf1b41b
ANDRES KWAN <andreskwan@gmail.com>
2015-04-22 06:37:11 -0500

    REST - GET - ID - working

--- Mantle+NSURLSession/ViewController.m
+++ Mantle+NSURLSession/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+static NSString* const kBaseURL = @"http://localhost:3000/productos/553784f9fa2899b67806287f";
 
 @interface ViewController ()
 
@@ -25,5 +26,57 @@
 }
 
 - (IBAction)getButton:(UIButton *)sender {
+    NSLog(@"Button pressed");
+    //Warning: REST add URL to persist data
+    NSURL* url = [NSURL URLWithString:kBaseURL];
+    
+    //    #warning REST create the request with the url
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    //    #warning REST PUT update or POST create
+    //    request.HTTPMethod = isExistingLocation ? @"PUT" : @"POST"; //2
+    request.HTTPMethod =@"GET";
+    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"]; //4
+    
+    //id
+    NSURLSessionConfiguration* config =
+    [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:config];
+    
+    NSURLSessionDataTask* dataTask =
+    [session dataTaskWithRequest:request
+               completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) { //5
+                   if (error == nil) {
+                       NSError *error;
+                       NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data
+                                                                                options:0
+                                                                                  error:&error];
+                       for (id obj in jsonDict)
+                       {
+//                           self.dict1 = obj;
+                           //here obj is a dictionary too
+                           NSLog(@"obj class:%@", [obj class]);
+                           //                NSLog(@"objt: %@",obj);
+                           //                i+=1;
+                           //                //here obj is a key of the obj dictionary
+//                                           for (id obj2 in obj) {
+//                                             NSLog(@"obj2 class:%@", [obj2 class]);
+                           //                    NSLog(@"\nobj2 key: %@\n",obj2);
+                           //                    NSLog(@"obj2 value: %@", [obj objectForKey:obj2]);
+                           //
+//                                           }
+                       }
+                       
+                       //            NSLog(@"%@", );
+                       //             NSArray* responseArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+                       //            if ([responseArray count])
+                       //            {
+                       //                self.label.text = [jsonDict objectForKey:<#(id)#>;
+                       //            }
+                   }
+               }];
+    [dataTask resume]; //8
+//    self.label.text = [self.dict1 objectForKey:@"title"];
+//    self.label.text = [self.dict1 objectForKey:@"title"];
 }
 @end

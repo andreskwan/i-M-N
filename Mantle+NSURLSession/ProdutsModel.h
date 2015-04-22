74864dd686caca6a18e120f855fedd111008aeda
ANDRES KWAN <andreskwan@gmail.com>
2015-04-22 06:51:41 -0500

    MANTLE - pod - added - project working!

---
+++ Mantle+NSURLSession/ProdutsModel.h
@@ -0,0 +1,14 @@
+//
+//  ProdutsModel.h
+//  Mantle+NSURLSession
+//
+//  Created by Andres Kwan on 4/22/15.
+//  Copyright (c) 2015 Kwan Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Mantle.h>
+
+@interface ProdutsModel : MTLModel <MTLJSONSerializing>
+
+@end
---
+++ Mantle+NSURLSession/ProdutsModel.m
@@ -0,0 +1,13 @@
+//
+//  ProdutsModel.m
+//  Mantle+NSURLSession
+//
+//  Created by Andres Kwan on 4/22/15.
+//  Copyright (c) 2015 Kwan Castle. All rights reserved.
+//
+
+#import "ProdutsModel.h"
+
+@implementation ProdutsModel
+
+@end
